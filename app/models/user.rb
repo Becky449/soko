e5630bee3468d07bccea0a759374f1677c66543b
Becky449 <rabnab12@gmail.com>
2024-11-16 23:18:34 +0300

    Modify User model to define roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         enum role: { admin: 'admin', customer: 'customer' }
+         after_initialize :set_default_role, if: :new_record?
+       
+         private
+         def set_default_role
+           self.role ||= :customer
+         end
 end
